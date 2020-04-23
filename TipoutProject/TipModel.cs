c3334c4d7f63526c4a680e8590ad08bc15234791
hellzhammer <mattperley@hotmail.com>
2020-04-23 01:37:05 -0400

    added some changes to the tip model. fixed a couple minor bugs. api is in working state.

--- TipoutProject/TipModel.cs
+++ TipoutProject/TipModel.cs
@@ -9,7 +9,10 @@ namespace TipoutProject
         public double TipoutAmount { get; set; }
         public string Workplace { get; set; }
         public DateTime ShiftDate { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
         public double TipPercentage { get; set; }
         public string ownerID { get; set; }
+        public bool BarTips { get; set; }
     }
 }
